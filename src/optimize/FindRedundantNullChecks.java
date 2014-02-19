d21acd7bf27800b7918f46996801b680f594495c
khaledalturkestani <k.turkestani@gmail.com>
2014-02-18 18:11:27 -0800

    Added implementation details of FindRedundantNullChecks in optimize/design.txt.

--- src/optimize/FindRedundantNullChecks.java
+++ src/optimize/FindRedundantNullChecks.java
@@ -2,6 +2,7 @@ package optimize;
 
 import joeq.Class.jq_Class;
 import joeq.Main.Helper;
+import flow.Flow;
 
 public class FindRedundantNullChecks {
 
@@ -12,5 +13,12 @@ public class FindRedundantNullChecks {
      */
     public static void main(String[] args) {
         //fill me in
+		String[] mySolverArgs  = new String[3];
+		mySolverArgs[0] = "submit.MySolver";
+		mySolverArgs[1] = "flow.ConstantProp"; //TODO: change this to FindRedundantNullChecks;
+		for (int i = 0; i < args.size(); i++) {
+			mySolverArgs[2] = args[i];
+			Flow.main(mySolverArgs);
+		}	
     }
 }
--- src/submit/MySolver.java
+++ src/submit/MySolver.java
@@ -1,5 +1,5 @@
 package submit;
-
+// Testing
 // some useful things to import. add any additional imports you need.
 import joeq.Compiler.Quad.*;
 import flow.Flow;
@@ -36,7 +36,6 @@ public class MySolver implements Flow.Solver {
 		} else {
 			processBackward(cfg);
 		}
-       
     }
 	
 	private void processForward(ControlFlowGraph cfg) {
